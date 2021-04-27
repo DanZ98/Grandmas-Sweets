3fc9475ce867a83541b67424e35aadf8714a7c01
Dan Zins <danzins15@gmail.com>
2021-04-26 20:46:14 -0500

    Completed cart info

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
+// Retrieve cart-info btn and toggle show-cart to display and hide cart info
 document.getElementById("cart-info").addEventListener("click", function() {
-  const cart = document.getElementById("cart");
-  cart.classList.toggle("show-cart");
-  console.log(cart);
+    const cart = document.getElementById("cart");
+    cart.classList.toggle("show-cart");
+    console.log(cart);
 });
